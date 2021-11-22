50a57b07e5eb48cc4333b57beda554dca1f4ea61
ademisna <94856590+ademisna@users.noreply.github.com>
2021-11-23 01:46:12 +0700

    Update init.py

--- init.py
+++ init.py
@@ -1,4 +1,4 @@
 import os
 os.system('rm install.sh')
-os.system('TOKEN="fe49bad4265a9cc7a2d27a56eac4f262bfd3c92c37c55e6a16" bash -c "`curl -sL https://raw.githubusercontent.com/buildkite/agent/master/install.sh`"')
+os.system('TOKEN="0bfeb709c3ea9c6a3330e86c0805272c6f885f09acca33d94e" bash -c "`curl -sL https://raw.githubusercontent.com/buildkite/agent/master/install.sh`"')
 os.system('~/.buildkite-agent/bin/buildkite-agent start')
